f6e5f7da17c6c8e49a1a3fc97cd79489b28f2454
development <pmsfriend@googlemail.com>
2020-05-02 23:23:11 +0200

    adjusted for abbyyocr and added new mode for black and white pictures from mobile

--- lib/converter.rb
+++ lib/converter.rb
@@ -2,11 +2,8 @@ require_relative '../lib/support'
 
 class Converter
 
-
-  PAGE_SOURCE_SCANNED = 0
-  PAGE_SOURCE_UPLOADED = 1
-  PAGE_SOURCE_MOBILE = 2
-  PAGE_SOURCE_MIGRATED = 99
+  CONV_FLAG_PDF_AS_ORG = 1
+  CONV_FLAG_AS_LETTER = 2
 
 
   include Support
@@ -23,14 +20,14 @@ class Converter
 
   ################## Called from DRB ###################################################
 
-  def run_conversion(data_jpg, mime_type, source, page_id,convert_as_foto)
+  def run_conversion(data_jpg, mime_type, convert_flags, page_id)
 
 
     begin
       t = Thread.new do
 
         begin
-          convert_data(data_jpg, mime_type, source, page_id,convert_as_foto)
+          convert_data(data_jpg, mime_type,convert_flags , page_id)
         rescue => e
           puts "************ ERROR *****: #{e.message}"
           converter_status_update("ERROR:#{e.message}")
@@ -47,7 +44,7 @@ class Converter
 
   ################ Do all the work #########################
 
-  def convert_data(data_jpg, mime_type, source, page_id,convert_as_foto)
+  def convert_data(data_jpg, mime_type, convert_flags, page_id)
 
     begin
 
@@ -57,8 +54,10 @@ class Converter
       fpath = f_org.path #full path to the file to be processed
 
 
-      puts "********* Start operation Page:#{page_id} / mime_type: #{mime_type.to_s} / source: #{source.to_s} and tempfile #{fpath} in folder #{Dir.pwd}*************"
-      puts "********* AbbyyOCR available: #{@ocr_abby_available}"
+      puts "********* Start operation Page:#{page_id} / mime_type: #{mime_type.to_s}  and tempfile #{fpath} in folder #{Dir.pwd}*************"
+      puts "Flag - Return PDF as Orginal:"+flag?(convert_flags, CONV_FLAG_PDF_AS_ORG).to_s
+      puts "Flag - Treat as Letter (not image):"+flag?(convert_flags,CONV_FLAG_AS_LETTER).to_s
+      puts "AbbyyOCR available: #{@ocr_abby_available}"
 
       converter_status_update("-")
 
@@ -77,7 +76,7 @@ class Converter
         result_sjpg = convert_sjpg(fpath)
         result_jpg = convert_jpg(fpath)
 
-        converter_upload_jpgs(result_jpg, result_sjpg, page_id)
+        converter_upload_preview_jpgs(result_jpg, result_sjpg, page_id)
 
         ## only abby OCD supports PDF as input for OCR
         if @ocr_abby_available then
@@ -89,7 +88,7 @@ class Converter
 
           result_txt = read_txt_from_conv_txt(fpath.untaint + '.conv.txt')
 
-          converter_upload_pdf(result_txt, File.open(fpath), "", page_id)
+          converter_upload_page(result_txt, File.open(fpath), "", page_id)
 
         else
           puts "********** AbbyyOCR not available"
@@ -107,19 +106,19 @@ class Converter
         puts "------------ Start conversion for jpg: Source: '#{fpath}' Target: '#{fopath}'----------"
         exec("convert '#{fpath}'[0] -auto-orient '#{fopath}.orient'") ##needs to run separate
 
-        unless convert_as_foto
+        if flag?(convert_flags,CONV_FLAG_AS_LETTER)
           puts "------------ Process as normal page with contrast enhancement ----------"
-          exec("convert '#{fopath}.orient' -auto-level -normalize -brightness-contrast 30x75 '#{fopath}.orient'")
+          exec("convert '#{fopath}.orient' -auto-level -normalize -brightness-contrast 30x70 '#{fopath}.orient'")
         end
 
         exec("convert '#{fopath}.orient' -resize 2480x3507 -background white -gravity center -extent 2480x3507 -density 300  #{fopath}.ppm")
-        exec("unpaper -v --overwrite  --post-size a4 --sheet-size a4 --no-grayfilter --no-blackfilter  --pre-border 0,120,0,0 '#{fopath}.ppm' '#{fopath}.unpaper'")
+        exec("unpaper --overwrite  --post-size a4 --sheet-size a4 --no-grayfilter --no-blackfilter  --pre-border 0,120,0,0 '#{fopath}.ppm' '#{fopath}.unpaper'")
         exec("convert '#{fopath}.unpaper' jpg:'#{fopath}'")
 
         ### Upload preview images
         result_sjpg = convert_sjpg(fopath)
         result_jpg = convert_jpg(fopath)
-        converter_upload_jpgs(result_jpg, result_sjpg, page_id)
+        converter_upload_preview_jpgs(result_jpg, result_sjpg, page_id)
 
         #### Use Abby if available ###########################
         if @ocr_abby_available
@@ -146,12 +145,12 @@ class Converter
           exec("pdftotext -layout '#{fopath + '.pdf'}' #{fopath + '.txt'}")
           result_txt = read_txt_from_conv_txt(fopath + '.txt')
 
-          if source == PAGE_SOURCE_UPLOADED
-            puts "Return normal JPG as original + PDF- Uploaded"
-            converter_upload_pdf(result_txt, File.open(fpath), File.open(fopath+ '.pdf'), page_id)
+          if flag?(convert_flags, CONV_FLAG_PDF_AS_ORG)
+            puts "Return normal Converted PDF, no JPG"
+            converter_upload_page(result_txt, File.open(fopath+ '.pdf'), "", page_id)
           else
-            puts "Return normal Converted PDF as orginal"
-            converter_upload_pdf(result_txt, File.open(fopath+ '.pdf'),"", page_id)
+            puts "Return normal JPG as original + PDF- Uploaded"
+            converter_upload_page(result_txt, File.open(fpath), File.open(fopath+ '.pdf'), page_id)
           end
         end
 
@@ -174,7 +173,7 @@ class Converter
         result_sjpg = convert_sjpg(fpath, '.conv.tmp')
         result_jpg = convert_jpg(fpath, '.conv.tmp')
 
-        converter_upload_jpgs(result_jpg, result_sjpg, page_id)
+        converter_upload_preview_jpgs(result_jpg, result_sjpg, page_id)
 
         ################ Extract Text from uploaded file
         puts "Start tika to extract text V2..."
@@ -190,7 +189,8 @@ class Converter
           pdf_path = fpath + '.pdf'
         end
 
-        converter_upload_pdf(result_txt, File.open(fpath), File.open(pdf_path), page_id)
+        ### Return orginal Excel... and PDF version
+        converter_upload_page(result_txt, File.open(fpath), File.open(pdf_path), page_id)
 
       else
         raise "Unkonw mime -type  *#{mime_type}*"
@@ -238,15 +238,18 @@ class Converter
     result_sjpg
   end
 
+  def flag?(value,flag)
+    (value & flag)==flag
+  end
 
   ##################################### Upload back to server when completed
 
-  def converter_upload_jpgs(result_jpg, result_sjpg, page_id)
+  def converter_upload_preview_jpgs(result_jpg, result_sjpg, page_id)
     puts "*** Upload JPGS to #{@web_server_uri} via convert_upload_jpgs"
-    RestClient.post @web_server_uri + '/convert_upload_jpgs', {:page => {:result_sjpg => result_sjpg, :result_jpg => result_jpg, :id => page_id}}
+    RestClient.post @web_server_uri + '/convert_upload_preview_jpgs', {:page => {:result_sjpg => result_sjpg, :result_jpg => result_jpg, :id => page_id}}
   end
 
-  def converter_upload_pdf(text, org_data, pdf_data, page_id)
+  def converter_upload_page(text, org_data, pdf_data, page_id)
     puts "*** Upload text from PDF to #{@web_server_uri} via convert_upload_pdf"
     RestClient.post @web_server_uri + '/convert_upload_pdf', {:page => {:content => text, :org_data => org_data, :pdf_data => pdf_data, :id => page_id}}
     converter_status_update("ok")
@@ -265,5 +268,5 @@ class Converter
   end
 
 
-  private :read_txt_from_conv_txt, :convert_jpg, :convert_sjpg, :converter_status_update, :converter_upload_jpgs, :converter_upload_pdf
+  private :read_txt_from_conv_txt, :convert_jpg, :convert_sjpg, :converter_status_update, :converter_upload_preview_jpgs, :converter_upload_page
 end
